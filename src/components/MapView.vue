22c260f8bef7516245aa56c8dbfbb6c6f1e5c820
Santiago Riano <santiagoriano68@gmail.com>
2025-02-01 09:06:03 -0500

    marcadores

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,25 +1,65 @@
 <template>
   <div class="map-container">
-    <l-map ref="map" v-model:zoom="zoom" :center="center" :use-global-leaflet="true">
+    <l-map ref="map" v-model:zoom="zoom" :center="center" :use-global-leaflet="true" @click="handleMapClick">
       <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
         name="OpenStreetMap"></l-tile-layer>
+
+      <!-- Marcador de la bodega -->
+      <l-marker :lat-lng="warehousePosition">
+        <l-icon :icon-url="warehouseIcon" :icon-size="[35, 40]" />
+        <l-popup>
+          <strong>Bodega (Punto de partida)</strong><br>
+          Lat: {{ warehousePosition[0] }}<br>
+          Lng: {{ warehousePosition[1] }}
+        </l-popup>
+      </l-marker>
+
+      <!-- Marcadores de puntos de entrega -->
+      <l-marker v-for="(marker, index) in markers" :key="index" :lat-lng="marker.position">
+        <l-icon :icon-url="deliveryIcon" :icon-size="[30, 35]" />
+        <l-popup>
+          Punto de entrega #{{ index + 1 }}<br>
+          Lat: {{ marker.position[0].toFixed(4) }}<br>
+          Lng: {{ marker.position[1].toFixed(4) }}
+        </l-popup>
+      </l-marker>
     </l-map>
   </div>
 </template>
 
 <script>
-import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
+import { LMap, LTileLayer, LMarker, LPopup, LIcon } from '@vue-leaflet/vue-leaflet'
 
 export default {
   name: 'MapView',
   components: {
     LMap,
-    LTileLayer
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LIcon
+  },
+  props: {
+    markers: {
+      type: Array,
+      default: () => []
+    }
   },
   data() {
     return {
-      zoom: 13,
+      zoom: 16,
       center: [4.757786586246297, -74.04488664305592],
+      warehousePosition: [4.757786586246297, -74.04488664305592],
+      warehouseIcon: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+      deliveryIcon: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png'
+    }
+  },
+  methods: {
+    handleMapClick(event) {
+      const { lat, lng } = event.latlng
+      this.$emit('marker-added', {
+        position: [lat, lng]
+      })
     }
   }
 }
--- src/views/RutasView.vue
+++ src/views/RutasView.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="rutas-container">
     <div class="map-section">
-      <MapView />
+      <MapView @marker-added="handleMarkerAdded" :markers="markers" ref="mapView" />
     </div>
     <div class="controls-section">
       <h2>Panel de Control</h2>
-      <p>Próximamente: Gestión de marcadores</p>
+      <div class="warehouse-info">
+        <h3>Punto de Partida (Bodega)</h3>
+        <p>{{ warehousePosition[0].toFixed(4) }}, {{ warehousePosition[1].toFixed(4) }}</p>
+      </div>
+      <div v-if="markers.length > 0">
+        <h3>Puntos de Entrega</h3>
+        <ul class="marker-list">
+          <li v-for="(marker, index) in markers" :key="index" class="marker-item">
+            <div class="marker-info">
+              Punto #{{ index + 1 }}:
+              {{ marker.position[0].toFixed(4) }},
+              {{ marker.position[1].toFixed(4) }}
+            </div>
+            <button @click="removeMarker(index)" class="delete-btn">
+              ×
+            </button>
+          </li>
+        </ul>
+      </div>
+      <p v-else>
+        Haga clic en el mapa para añadir puntos de entrega
+      </p>
     </div>
   </div>
 </template>
@@ -17,6 +38,20 @@ export default {
   name: 'RutasView',
   components: {
     MapView
+  },
+  data() {
+    return {
+      markers: [],
+      warehousePosition: [4.757786586246297, -74.04488664305592]
+    }
+  },
+  methods: {
+    handleMarkerAdded(marker) {
+      this.markers.push(marker)
+    },
+    removeMarker(index) {
+      this.markers.splice(index, 1)
+    }
   }
 }
 </script>
@@ -43,5 +78,59 @@ export default {
   padding: 20px;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.marker-list {
+  list-style: none;
+  padding: 0;
+}
+
+.marker-list li {
+  padding: 8px 0;
+  border-bottom: 1px solid #eee;
+}
+
+.warehouse-info {
+  background-color: #f8f7d7;
+  border: 1px solid #f4f5c6;
+  border-radius: 4px;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+
+.warehouse-info h3 {
+  color: #725c1c;
+  margin-top: 0;
+}
+
+.marker-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px;
+  border-bottom: 1px solid #eee;
+}
+
+.marker-info {
+  flex-grow: 1;
+}
+
+.delete-btn {
+  background-color: #dc3545;
+  color: white;
+  border: none;
+  border-radius: 50%;
+  width: 24px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  font-size: 16px;
+  margin-left: 10px;
+}
+
+.delete-btn:hover {
+  background-color: #c82333;
 }
 </style>
