480690e4479ae0de89438d94a1f16800fecbe400
Afom04 <andrey.felipe.orozco.montoya@gmail.com>
2025-02-05 19:54:22 -0500

    Añadiendo CRUD a DRIVERS

--- src/routes/drivers.js
+++ src/routes/drivers.js
@@ -51,5 +51,127 @@ router.get("/available", async (req, res, next) => {
     next(error);
   }
 });
+// GET api/drivers/
+router.get("/",async (req,res,next)=>{
+  try {
+    const result = await pool.query(
+      `
+        SELECT id, first_name, identification ,last_name, license_number, phone, created_at
+        FROM drivers 
+        `
+    );
+    res.json({
+      success: true,
+      data: result.rows,
+    });
+  } catch (error) {
+    console.error('Error in /available:', error);
+    next(error);
+  }
+})
+
+// GET /api/drivers/:identification
+router.get("/:identification", async (req, res, next) => {
+  try {
+    const { identification } = req.params;
+    console.log("Request",req.params);
+    const result = await pool.query(
+      `
+        SELECT id, first_name, last_name, identification, license_number, phone, email, created_at
+        FROM drivers 
+        WHERE identification = $1
+        `,
+      [identification]
+    );
+    // console.log(result);
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Driver not found",
+      });
+    }
+    res.json({
+      success: true,
+      data: result.rows[0],
+    });
+  } catch (error) {
+    console.error('Error in /:id:', error);
+    next(error);
+  }
+});
+
+// POST /api/drivers/
+router.post("/",async(req, res,next)=> {
+  try {
+    const { identification,first_name, last_name, license_number, phone, email } = req.body;
+    if(!identification || !first_name || !last_name|| !license_number || !phone || !email){
+      return res.status(400).json({
+        success: false,
+        message: "Todos los datos son necesarios",
+      });
+    }
+    // Validar que el número de licencia no haya sido utilizado anteriormente
+    
+    const result = await pool.query(
+      `
+        INSERT INTO drivers (identification,first_name, last_name, license_number, phone, email)
+        VALUES ($1, $2, $3, $4, $5,$6)
+        RETURNING id
+        `,
+      [identification,first_name, last_name, license_number, phone, email]
+    );
+    res.status(201).json({
+      success: true,
+      data: result.rows[0],
+    });
+  } catch (error) {
+    console.error('Error in /post:', error);
+    next(error);
+  }
+});
+
+// PUT /api/drivers/:identification
+
+router.put("/:identification", async (req, res,next) => {
+  try {
+    const { identification,first_name, last_name, phone, email } = req.body;
+    if(!identification || !first_name || !last_name || !phone || !email){
+      return res.status(400).json({
+        success: false,
+        message: "Todos los datos son necesarios",
+      });
+    }
+    // Validar que el número de licencia no haya sido utilizado anteriormente
+    const validatePhone = await pool.query(
+      `
+        SELECT COUNT(*) AS count
+        FROM drivers
+        WHERE phone = $1 AND identification!= $2`,
+        [phone, identification]
+    );
+    if (validatePhone.rows[0].count > 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Número de licencia ya está en uso",
+      });
+    }
+    console.log("Telefono valido");
+    // Actualizar los datos del conductor
+    const result = await pool.query(
+      `
+        UPDATE drivers
+        SET first_name = $2, last_name = $3, phone = $4, email = $5
+        WHERE identification = $1`,
+        [identification, first_name, last_name, phone, email]
+    );
+    res.status(201).json({
+      success: true,
+      data: result.rows[0],
+    });
+  } catch (error) {
+    console.error('Error in PUT/:', error);
+    next(error);
+  }
+});
 
 export default router;
